d4bb8d7d1b1518ab02ba056b3c49246e49553788
Natalia Maximo <natalia.maximo@uottawa.ca>
2021-01-22 13:35:16 -0500

    chg: added tests

--- tests/lib_test.py
+++ tests/lib_test.py
@@ -1,12 +1,14 @@
+# -*- coding: utf-8 -*-
 import pytest
 import fsa_bot.lib as lib
-import sys
-import io
 
 
 @pytest.mark.lib
-def test_main(capsys):
-    lib.main()
-    captured = capsys.readouterr()
-
-    assert captured.out == 'Hello, World!\n'
+@pytest.mark.parametrize('csv_string,out', [
+    (
+        'a,x,y b,x,z a,y,x b,y,z a,z,z b,z,z',
+        [{'trigger': 'a', 'source': 'x', 'target': 'y'}, {'trigger': 'b', 'source': 'x', 'target': 'z'}, {'trigger': 'a', 'source': 'y', 'target': 'x'}, {'trigger': 'b', 'source': 'y', 'target': 'z'}, {'trigger': 'a', 'source': 'z', 'target': 'z'}, {'trigger': 'b', 'source': 'z', 'target': 'z'}]  # noqa: E501
+    )
+])
+def test_csv_convert(csv_string, out):
+    assert out == lib.csv_string_to_dicts(csv_string)
